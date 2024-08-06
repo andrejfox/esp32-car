7e6d6581e2b429fddf90f5c2529f3068ca840749
Andrej Vencelj <andrej.vencelj.007@gmail.com>
2024-08-07 00:12:15 +0200

    fixed skillissue

--- src/main.cpp
+++ src/main.cpp
@@ -57,7 +57,7 @@ void connectToMQTT() {
     }
 }
 
-void mqttCallback(char *topic, byte *payload, unsigned int length) {
+void mqttCallback(char* topic, byte* payload, unsigned int length) {
     String message = "";
     for (int i = 0; i < length; i++) {
         message += (char)payload[i];
