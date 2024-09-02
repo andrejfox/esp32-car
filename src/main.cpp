39f972d61eb11ae4a7a2987d0ef037e6f45ce24b
Andrej Vencelj <andrej.vencelj.007@gmail.com>
2024-09-02 17:58:59 +0200

    updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,29 +2,126 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 
-const int motorAL = 15;
-const int motorAR = 16;
-const int motorBL = 17;
-const int motorBR = 18;
+// WiFi nastavitve
+const char *ssid = "zavod404"; // Ime WiFi-ja
+const char *password = "zavod404"; // Geslo WiFi-ja
 
-const int speeeeed = 50; //0 - 255
-
-// WiFi credentials
-const char *ssid = "kabum2";             // Replace with your WiFi name
-const char *password = "skratek2";     // Replace with your WiFi password
-
-// MQTT Broker settings
-const char *mqtt_broker = "213.172.234.74";
-const char *mqtt_topic = "esp/marcel";
-const char *mqtt_username = "pp";
+// MQTT Broker nastavitve
+const char *mqtt_broker = "213.172.234.74"; // naslov / IP
+const char *mqtt_topic = "esp/test";
+const char *mqtt_username = "pp"; 
 const char *mqtt_password = "pp";
 const int mqtt_port = 1883;
 
-// WiFi and MQTT client initialization
+// pini
+int motorL_1 = 15;
+int motorL_2 = 16;
+int motorD_1 = 17;
+int motorD_2 = 18;
+int trigPin = 4;
+int echoPin = 5;
+int lucka = 9;
+
+// globalne spremenlivke
+int hitrost = 200;
+float duration, distance;
+bool automatic_mode = false;
+
+// WiFi and MQTT client inicializacija
 WiFiClient esp_client;
 PubSubClient mqtt_client(esp_client);
 
-void connectToWiFi() {
+void naprej() {
+    analogWrite(motorD_1, hitrost);
+    analogWrite(motorD_2, LOW);
+    analogWrite(motorL_1, LOW);
+    analogWrite(motorL_2, hitrost);
+}
+
+void nazaj() {
+    analogWrite(motorD_1, LOW);
+    analogWrite(motorD_2, hitrost);
+    analogWrite(motorL_1, hitrost);
+    analogWrite(motorL_2, LOW);
+}
+
+void stop() {
+    analogWrite(motorD_1, LOW);
+    analogWrite(motorD_2, LOW);
+    analogWrite(motorL_1, LOW);
+    analogWrite(motorL_2, LOW);
+}
+
+void levi_nazaj() {
+    analogWrite(motorD_1, LOW);
+    analogWrite(motorD_2, hitrost);
+    analogWrite(motorL_1, LOW);
+    analogWrite(motorL_2, LOW);
+}
+
+void levi_naprej() {
+    analogWrite(motorD_1, hitrost);
+    analogWrite(motorD_2, LOW);
+    analogWrite(motorL_1, LOW);
+    analogWrite(motorL_2, LOW);
+}
+
+void desni_nazaj() {
+    analogWrite(motorD_1, LOW);
+    analogWrite(motorD_2, LOW);
+    analogWrite(motorL_1, hitrost);
+    analogWrite(motorL_2, LOW);
+}
+
+void desni_naprej() {
+    analogWrite(motorD_1, LOW);
+    analogWrite(motorD_2, LOW);
+    analogWrite(motorL_1, LOW);
+    analogWrite(motorL_2, hitrost);
+}
+
+void obrni_levo(int cas_obracanja) {
+    analogWrite(motorD_1, LOW);
+    analogWrite(motorD_2, hitrost);
+    analogWrite(motorL_1, LOW);
+    analogWrite(motorL_2, hitrost);
+    delay(cas_obracanja);
+}
+
+void obrni_desno(int cas_obracanja) {
+    analogWrite(motorD_1, hitrost);
+    analogWrite(motorD_2, LOW);
+    analogWrite(motorL_1, hitrost);
+    analogWrite(motorL_2, LOW);
+    delay(cas_obracanja);
+}
+
+void automatic() { // Automatsko premikanje
+    do {
+        digitalWrite(trigPin, LOW);
+        delayMicroseconds(2);
+        digitalWrite(trigPin, HIGH);
+        delayMicroseconds(10);
+        digitalWrite(trigPin, LOW);
+
+        duration = pulseIn(echoPin, HIGH);
+        distance = (duration * 0.0343) / 2;
+        Serial.print("Distance: ");
+        Serial.println(distance);
+        delay(100);
+
+        if (distance < 10) {
+            nazaj();
+            delay(500);
+            obrni_levo(1000);
+        } else {
+            naprej();
+        }
+        mqtt_client.loop();
+    } while (automatic_mode == true);
+}
+
+void connectToWiFi() { // Povezovanje z internetom
     WiFi.begin(ssid, password);
     Serial.println();
     Serial.println("Connecting to WiFi");
@@ -37,10 +134,9 @@ void connectToWiFi() {
         Serial.print(".");
     }
     Serial.println("\nConnected to WiFi\n");
-
 }
 
-void connectToMQTT() {
+void connectToMQTT() { // povezovanje z MQTT brockerjem
     while (!mqtt_client.connected()) {
         String client_id = "esp32-client-" + String(WiFi.macAddress());
         Serial.printf("Connecting to Broker as [%s]\n", client_id.c_str());
@@ -64,57 +160,64 @@ void connectToMQTT() {
     }
 }
 
-void mqttCallback(char* topic, byte* payload, unsigned int length) {
+void mqttCallback(char *topic, byte *payload, unsigned int length) {
     String message = "";
-    for (int i = 0; i < length; i++) {
+    for (int i = 0; i < length; i++) { // Sestavimo spozočilo
         message += (char)payload[i];
     }
-    
+
     Serial.print(topic);
     Serial.print(": ");
     Serial.println(message);
 
-    if (message == "naprej") {
-        analogWrite(motorAL, speeeeed);
-        analogWrite(motorAR, 0);
-        analogWrite(motorBL, speeeeed);
-        analogWrite(motorBR, 0);
-        delay(1000);
-        analogWrite(motorAL, 0);
-        analogWrite(motorAR, 0);
-        analogWrite(motorBL, 0);
-        analogWrite(motorBR, 0);
+    if (message == "naprej") { // Preverimo kaj je sporočilo
+        automatic_mode = false;
+        Serial.println("naprej");
+        naprej();
     } else if (message == "nazaj") {
-        analogWrite(motorAL, 0);
-        analogWrite(motorAR, speeeeed);
-        analogWrite(motorBL, 0);
-        analogWrite(motorBR, speeeeed);
-        delay(1000);
-        analogWrite(motorAL, 0);
-        analogWrite(motorAR, 0);
-        analogWrite(motorBL, 0);
-        analogWrite(motorBR, 0);
+        automatic_mode = false;
+        nazaj();
+    } else if (message == "stop") {
+        automatic_mode = false;
+        stop();
+    } else if (message == "obrni") {
+        automatic_mode = false;
+        obrni_desno(500);
+    } else if (message == "auto") {
+        automatic_mode = true;
+        automatic();
     }
 }
 
-void setup() {
-    pinMode(motorAL, OUTPUT);
-    pinMode(motorAR, OUTPUT);
-    pinMode(motorBL, OUTPUT);
-    pinMode(motorBR, OUTPUT);
+void pinSetup() { // pine nastavimo na OUTPUT / INPUT
+    pinMode(motorD_1, OUTPUT);
+    pinMode(motorD_2, OUTPUT);
+    pinMode(motorL_1, OUTPUT);
+    pinMode(motorL_2, OUTPUT);
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+    pinMode(lucka, OUTPUT);
+}
 
-    Serial.begin(921600);
-    Serial.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+void setup() {
+    Serial.begin(115200);
+    for(int i = 0; i <= 20; i++) Serial.println("\n");
 
     connectToWiFi();
     mqtt_client.setServer(mqtt_broker, mqtt_port);
     mqtt_client.setCallback(mqttCallback);
     connectToMQTT();
+
+    pinSetup();
 }
 
-void loop() { 
+void loop() {
+    digitalWrite(lucka, HIGH);
+
     if (!mqtt_client.connected()) {
         connectToMQTT();
+        Serial.println("connecting");
     }
+    
     mqtt_client.loop();
 }
